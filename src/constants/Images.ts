cc99043fb05413cb32220cd338f3805ee90a1148
HabibaOrabi188 <habibaorabi57@gmail.com>
2024-10-21 23:20:16 +0300

    Uploud Project

---
+++ src/constants/Images.ts
@@ -0,0 +1,54 @@
+const IMAGES = {
+  HomeSVG: require('../assets/svg/home.svg'),
+  BLoomyLogo: require('../assets/images/logo.png'),
+  LoginPng: require('../assets/images/login.png'),
+  RegisterImg: require('../assets/images/register.png'),
+  Intro1: require('../assets/images/introo1.png'),
+  Intro2: require('../assets/images/introo2.png'),
+  Intro3: require('../assets/images/introo3.png'),
+  Intro4: require('../assets/images/introo4.png'),
+  Mothers: require('../assets/images/mother.png'),
+  Wedding: require('../assets/images/wedding.png'),
+  Graduation: require('../assets/images/Graduation.png'),
+  Birthday: require('../assets/images/Birthday.png'),
+  valentine: require('../assets/images/valentine.png'),
+  Soon: require('../assets/images/Soon.png'),
+  flower1: {
+    uri: 'https://i.pinimg.com/564x/55/1b/1b/551b1bde4c8fbe96687c812b01006ea3.jpg',
+  },
+  flower2: {
+    uri: 'https://gardentulipflowers.ae/wp-content/uploads/2023/08/GT-BQ-23-DP-1353F-1.jpg',
+  },
+  flower3: {
+    uri: 'https://i.pinimg.com/564x/5f/07/2e/5f072e07300dbfd5e8f66aed523cbf57.jpg',
+  },
+  flower5: {
+    uri: 'https://i.pinimg.com/564x/94/b9/81/94b981f31cf007c2ad3b7ea6fa2210dd.jpg',
+  },
+  flower6: {
+    uri: 'https://i.etsystatic.com/48805835/c/2000/2000/0/0/il/d95ffc/5728254861/il_300x300.5728254861_66ap.jpg',
+  },
+  flower7: {
+    uri: 'https://i.pinimg.com/736x/b0/87/6b/b0876be8ede7ca22de1e80088598bb54.jpg',
+  },
+  flower8: {
+    uri: 'https://flowerdeliverycanberra.com.au/wp-content/uploads/2021/09/get-well-soon-flowers.jpg',
+  },
+  flower9: {
+    uri: 'https://i.pinimg.com/originals/10/06/f2/1006f235b73252562040f891e949893c.jpg',
+  },
+  flower10: {
+    uri: 'https://i.pinimg.com/564x/00/38/61/0038616b03886587beb340a47a7235ee.jpg',
+  },
+  splash: require('../assets/images/logo.png'),
+  flower4: {
+    uri: 'https://www.partylicious.com.my/wp-content/uploads/2022/04/Bunch-of-Love-Carnation-Bouquet-Fresh-scaled.jpg',
+  },
+  VISa: require('../assets/images/Mastercard-Logo-PNG8.png'),
+  Paypal: require('../assets/images/PayPal.svg.png'),
+  Mastercard: require('../assets/images/Visa-Logo-PNG7.png'),
+  Success: require('../assets/images/check.png'),
+  CartEmpty: require('../assets/images/cartempty.png'),
+  Profile: require('../assets/images/profile.jpg'),
+};
+export default IMAGES;
